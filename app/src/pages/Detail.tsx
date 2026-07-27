60cbc84f26213825472ebd76ca66d042a8b35ac0
Steamix <steamix@steamix.com>
2026-07-27 11:31:22 +0300

    episod Steamix: tum mantik DetailView icinde, click aninda isMobile+server kontrolu

--- app/src/pages/Detail.tsx
+++ app/src/pages/Detail.tsx
@@ -142,23 +142,6 @@ export default function Detail() {
     })
     setIsFav(nowFav)
   }
-
-  const handleEpisodeClick = (ep: any) => {
-    const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
-    if (isMobile && server) {
-      const { base_url, xtream_user, xtream_pass } = server
-      window.location.href = buildSteamixIntentUrl(base_url, xtream_user, xtream_pass, parseInt(String(ep.stream_id || ep.id)), ep.container_extension || ext, {
-        type: 'series', season: ep.season || '1', episode: ep.episode_num || '1'
-      })
-      return
-    }
-    const sp = new URLSearchParams({ stream_id: String(ep.stream_id || ep.id), type: 'series', season: ep.season || '1', episode: ep.episode_num })
-    if (ep.container_extension) sp.set('ext', ep.container_extension)
-    if (data?.stream_icon) sp.set('icon', data.stream_icon)
-    sp.set('series_id', String(data?.id || id))
-    navigate(`/watch?${sp}`)
-  }
-
   if (loading) {
     return (
       <div className="min-h-screen bg-[#0f172a] flex items-center justify-center">
@@ -182,7 +165,6 @@ export default function Detail() {
       onSimilarClick={handleSimilarClick}
       isFav={isFav}
       onToggleFav={handleToggleFav}
-      onEpisodeClick={handleEpisodeClick}
       onPlay={() => {
         const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
         if (isMobile && (type === 'movie' || type === 'series') && server) {
